bd53ecc88fdfef34cefb44655d2b5b85a64cde22
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-26 05:30:17 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 def my_all?(collection)
   i=0
+  return_val = []
   while i < collection.length
-    yield(collection[i])
+    return_val << yield(collection[i])
     i +=1
   end
-
+return_val
 end
